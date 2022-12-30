e3a33c518d60af7cb3d43aac6d15fa0b0edde7c1
Miguel Lopez <ing.miguellv@gmail.com>
2022-12-30 11:49:52 -0600

    agrego partials

--- app.js
+++ app.js
@@ -1,26 +1,41 @@
 const express = require('express')
+const hbs = require('hbs')
+
 const app = express()
 const port = 8080
 
 app.set('view engine', 'hbs')
+hbs.registerPartials( __dirname + '/views/partials' )
 
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
   // res.sendFile( __dirname + '/public/generic.html' )
-  res.render('home')
+  res.render('home', {
+    name: 'Miguel Lopez',
+    title: 'Handlebars'
+  })
 })
 
 app.get('/generic', (req, res) => {
-  res.sendFile( __dirname + '/public/generic.html' )
+  // res.sendFile( __dirname + '/public/generic.html' )
+  res.render('generic', {
+    name: 'Miguel Lopez',
+    title: 'Handlebars'
+  })
 })
 
 app.get('/elements', (req, res) => {
-  res.sendFile( __dirname + '/public/elements.html' )
+  // res.sendFile( __dirname + '/public/elements.html' )
+  res.render('elements', {
+    name: 'Miguel Lopez',
+    title: 'Handlebars'
+  })
 })
 
 app.get('*', (req, res) => {
-  res.sendFile( __dirname + '/public/404.html' )
+  // res.sendFile( __dirname + '/public/404.html' )
+  res.render('404')
 })
 
 app.listen(port, () => {

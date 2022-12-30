5275ffcd90ee6266da0230080a08fb9be324643f
Miguel Lopez <ing.miguellv@gmail.com>
2022-12-30 10:16:13 -0600

    agrego handlebars para renderizado de sitio web

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-console.log('hola mundo')
+const express = require('express')
+const app = express()
+const port = 8080
+
+app.set('view engine', 'hbs')
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  // res.sendFile( __dirname + '/public/generic.html' )
+  res.render('home')
+})
+
+app.get('/generic', (req, res) => {
+  res.sendFile( __dirname + '/public/generic.html' )
+})
+
+app.get('/elements', (req, res) => {
+  res.sendFile( __dirname + '/public/elements.html' )
+})
+
+app.get('*', (req, res) => {
+  res.sendFile( __dirname + '/public/404.html' )
+})
+
+app.listen(port, () => {
+  console.log(`Server UP from port ${port}`)
+})
